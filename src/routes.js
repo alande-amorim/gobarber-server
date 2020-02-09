626d8ec230fcfb4006fac3498a6619ecb24ac8a8
Alande Amorim <alande.amorim@gmail.com>
2020-02-09 09:52:39 -0300

    Listagem de agendamentos do usuário + paginação

--- src/routes.js
+++ src/routes.js
@@ -19,6 +19,7 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
 
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 export default routes;
