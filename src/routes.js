22ba83a6e9539601526c66f6a4e7430d6bb3ec9d
Alande Amorim <alande.amorim@gmail.com>
2020-02-09 10:14:37 -0300

    Agenda do prestador de serviço

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -21,5 +22,7 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedules', ScheduleController.index);
 
 export default routes;
